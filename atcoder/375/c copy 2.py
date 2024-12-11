481fa326df2c815009342f2c54974ada5c4645a3
SOTA702217 <nakano.sota7022@gmail.com>
2024-12-11 10:12:48 +0900

    Refactor rotation logic and remove obsolete file for AtCoder 375 problem

--- atcoder/375/c copy 2.py
+++ atcoder/375/c copy 2.py
@@ -2,15 +2,15 @@ N = int(input())
 A = [list(input()) for _ in range(N)]
 
 for i in range(N // 2):
-    A_copy=A
-    for j in range(i, N - i):
-        A[i][j] = A_copy[j][N - i-1]
-        A[j][i]=A_copy[i][N-j-1]
-        # for row in A_copy:
-        #     print(''.join(row))
-        # print(i,j)
-    for row in A:
-        print(''.join(row))
+    A_copy = [row[:] for row in A] 
+    for a in range(i, N - i):
+        for b in range(i, N - i):
+            A_copy[b][N-a-1] = A[a][b]
+    A = [row[:] for row in A_copy]
+    # print('=========================')
+    # for row in A:
+    #     print(''.join(row))
+
 
 for row in A:
     print(''.join(row))
